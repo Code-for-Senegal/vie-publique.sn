8024d8b50780162c0afb74318a727fa5391829a1
Malick <diop-malick@hotmail.fr>
2025-08-11 04:45:06 +0200

    ci/cd fix var on directus sdk

--- server/utils/directus.ts
+++ server/utils/directus.ts
@@ -1,15 +1,27 @@
 import { createDirectus, staticToken, rest, realtime } from "@directus/sdk";
 
-const directusUrl = process.env.CMS_API_URL || "https://cms.vie-publique.sn";
-const directusToken = process.env.CMS_API_KEY;
+let client: any = null;
 
-if (!directusToken) {
-  throw new Error("CMS_API_KEY environment variable is required");
+function getDirectusClient() {
+  if (!client) {
+    const directusUrl = process.env.CMS_API_URL || "https://cms.vie-publique.sn";
+    const directusToken = process.env.CMS_API_KEY;
+
+    if (!directusToken) {
+      throw new Error("CMS_API_KEY environment variable is required");
+    }
+
+    client = createDirectus(directusUrl)
+      .with(staticToken(directusToken))
+      .with(rest())
+      .with(realtime());
+  }
+  
+  return client;
 }
 
-const client = createDirectus(directusUrl)
-  .with(staticToken(directusToken))
-  .with(rest())
-  .with(realtime());
-
-export const directus = client;
+export const directus = new Proxy({}, {
+  get(target, prop) {
+    return getDirectusClient()[prop];
+  }
+});
