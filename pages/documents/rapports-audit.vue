2479a3478173a8bf59aaebb8c212dd56c936e5e5
#CISSE410 <dramisse90@gmail.com>
2025-09-13 17:08:54 +0000

    fix(search): recherche d'un document au niveau des rapports d'audits

--- pages/documents/rapports-audit.vue
+++ pages/documents/rapports-audit.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-const { documents, loading, error } = useDocuments({ type: "audit_report" });
-const router = useRouter();
-
 useHead({
-  title: "Rapports public Sénégal OFNAC Cours des compte",
+  title: "Rapports public Sénégal OFNAC Cours des compte IGE CENTIF",
   meta: [
     {
       name: "description",
@@ -13,24 +10,38 @@ useHead({
   ],
 });
 
+const rapports = ref<any[]>([]);
 const searchQuery = ref("");
 const selectedOrganisme = ref("");
+const selectedYear = ref("");
 
-const organismes = ["Cour des Comptes", "OFNAC", "CENTIF", "IGE", "ARMP"];
+const organismes = ["Cours des Comptes", "OFNAC", "CENTIF", "IGE", "ARMP"];
 
 const filteredRapports = computed(() =>
-  documents.value.filter((rapport) => {
+  rapports.value.filter((rapport) => {
     return (
       (searchQuery.value.length === 0 ||
-        rapport.title.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
-        rapport.description
+        rapport.titre.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+        rapport.sous_titre
           .toLowerCase()
           .includes(searchQuery.value.toLowerCase())) &&
       (selectedOrganisme.value === "" ||
-        rapport.audit_institution === selectedOrganisme.value)
+        rapport.organisme === selectedOrganisme.value) &&
+      (selectedYear.value === "" ||
+        rapport.annee === parseInt(selectedYear.value))
     );
   }),
 );
+
+onMounted(async () => {
+  rapports.value = (
+    await import("@/assets/data/rapport/rapports-liste.json")
+  ).default.sort(
+    (a, b) =>
+      new Date(b.date_publication).getTime() -
+      new Date(a.date_publication).getTime(),
+  );
+});
 
 /* Pagination */
 
@@ -51,177 +62,147 @@ watch(selectedOrganisme, () => {
 </script>
 
 <template>
-  <div class="container mx-auto px-4 py-4">
-    <!-- Bouton retour -->
-    <UButton
-      icon="i-heroicons-arrow-left"
-      variant="ghost"
-      label="Retour"
-      color="gray"
-      @click.native="router.back()"
-    />
-
+  <div class="flex flex-col items-center px-4">
     <h1 class="sr-only">
       Rapports d'audits publics Sénégal OFNAC Cour des comptes IGE CENTIF
     </h1>
 
-    <ClientOnly>
-      <div class="prose prose-sm sm:prose mx-auto my-2">
-        <h1 class="text-center text-xl text-gray-900 sm:text-2xl">
-          Rapports publics
-        </h1>
-      </div>
-      <p
-        v-if="rowsfilteredRapports.length > 1"
-        class="mb-4 text-center text-sm text-gray-500"
+    <div class="prose prose-sm sm:prose mx-auto my-2">
+      <h1 class="text-center dark:text-white">Rapports publics</h1>
+    </div>
+    <p v-if="rapports.length > 1" class="mb-4 text-sm text-gray-500">
+      {{ rapports.length }} rapports disponibles
+    </p>
+
+    <UInput
+      v-model="searchQuery"
+      size="md"
+      placeholder="Rechercher..."
+      icon="i-heroicons-magnifying-glass"
+      class="input custom-shadow mb-1 w-full"
+    />
+
+    <div class="my-3 w-full text-center">
+      <UButton
+        class="custom-shadow mb-1 ml-1"
+        :color="selectedOrganisme === '' ? 'primary' : 'white'"
+        @click="selectedOrganisme = ''"
       >
-        {{ documents.length }} rapports disponibles
-      </p>
+        Tous
+      </UButton>
+      <UButton
+        v-for="organisme in organismes"
+        :key="organisme"
+        class="custom-shadow mb-1 ml-1"
+        :color="selectedOrganisme === organisme ? 'primary' : 'white'"
+        @click="
+          selectedOrganisme = selectedOrganisme === organisme ? '' : organisme
+        "
+      >
+        {{ organisme }}
+      </UButton>
+    </div>
 
-      <UInput
-        v-model="searchQuery"
-        size="md"
-        placeholder="Rechercher..."
-        icon="i-heroicons-magnifying-glass"
-        class="input custom-shadow mb-1 w-full"
-      />
-
-      <div class="my-3 w-full text-center">
-        <UButton
-          class="custom-shadow mb-1 ml-1"
-          :color="selectedOrganisme === '' ? 'primary' : 'white'"
-          @click="selectedOrganisme = ''"
-        >
-          Tous
-        </UButton>
-        <UButton
-          v-for="organisme in organismes"
-          :key="organisme"
-          class="custom-shadow mb-1 ml-1"
-          :color="selectedOrganisme === organisme ? 'primary' : 'white'"
-          @click="
-            selectedOrganisme = selectedOrganisme === organisme ? '' : organisme
-          "
-        >
-          {{ organisme }}
-        </UButton>
+    <div class="flex flex-col gap-2">
+      <!-- Afficher USkeleton si les rapports ne sont pas encore chargés -->
+      <div v-if="filteredRapports.length == 0">
+        <div v-for="i in 5" :key="`skeleton-${i}`">
+          <USkeleton class="custom-shadow mb-2 h-24 w-full bg-white" />
+        </div>
       </div>
 
-      <template v-if="loading">
-        <UCard v-for="n in 3" :key="n" class="mb-4">
-          <div class="flex items-start gap-4 p-4">
-            <div class="h-8 w-8 animate-pulse rounded-full bg-gray-200" />
-            <div class="flex-grow">
-              <div class="mb-2 h-6 w-3/4 animate-pulse rounded bg-gray-200" />
-              <div class="h-4 w-1/2 animate-pulse rounded bg-gray-200" />
-            </div>
+      <!-- Afficher les cartes de rapport une fois chargées -->
+      <UCard
+        v-for="rapport in rowsfilteredRapports"
+        v-else
+        :key="rapport.id"
+        class="custom-shadow cursor-pointer"
+      >
+        <NuxtLink
+          :to="`/rapport-senegal/${rapport.slug}`"
+          class="flex flex-row gap-2"
+        >
+          <div class="w-12 flex-shrink-0 md:w-16">
+            <img
+              v-if="rapport.organisme == 'ARMP'"
+              src="~/assets/logos/armp.webp"
+              loading="lazy"
+              fetchpriority="high"
+              alt="Logo ARMP"
+              class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
+              width="60"
+              height="40"
+            />
+            <img
+              v-if="rapport.organisme == 'OFNAC'"
+              src="~/assets/logos/ofnac.webp"
+              loading="lazy"
+              fetchpriority="high"
+              alt="Logo OFNAC"
+              class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
+              width="60"
+              height="40"
+            />
+            <img
+              v-if="rapport.organisme == 'IGE'"
+              src="~/assets/logos/ige.webp"
+              loading="lazy"
+              fetchpriority="high"
+              alt="Logo IGE"
+              class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
+              width="60"
+              height="40"
+            />
+            <img
+              v-if="rapport.organisme == 'Cours des Comptes'"
+              src="~/assets/logos/cour_des_comptes.webp"
+              loading="lazy"
+              fetchpriority="high"
+              alt="Logo Cours des Comptes"
+              class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
+              width="60"
+              height="40"
+            />
+            <img
+              v-if="rapport.organisme == 'CENTIF'"
+              src="~/assets/logos/centif.webp"
+              loading="lazy"
+              fetchpriority="high"
+              alt="Logo CENTIF"
+              class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
+              width="60"
+              height="40"
+            />
+            <img
+              v-if="rapport.organisme == 'Autres'"
+              src="~/assets/logos/doc.svg"
+              loading="lazy"
+              fetchpriority="high"
+              alt="Logo rapport"
+              class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
+              width="60"
+              height="40"
+            />
           </div>
-        </UCard>
-      </template>
 
-      <UAlert
-        v-else-if="error"
-        title="Erreur"
-        color="red"
-        icon="i-heroicons-exclamation-triangle"
-      >
-        {{ error }}
-      </UAlert>
+          <div class="flex-grow">
+            <p class="text-sm font-semibold">{{ rapport.titre }}</p>
+            <p class="text-sm text-gray-500">{{ rapport.sous_titre }}</p>
+          </div>
+        </NuxtLink>
+      </UCard>
+    </div>
 
-      <div v-else class="flex flex-col gap-2">
-        <!-- Afficher les cartes de rapport une fois chargées -->
-        <UCard
-          v-for="rapport in rowsfilteredRapports"
-          :key="rapport.id"
-          class="custom-shadow cursor-pointer"
-        >
-          <NuxtLink
-            :to="`/documents/${rapport.id}/${rapport.slug}`"
-            class="flex flex-row gap-2"
-          >
-            <div class="w-12 flex-shrink-0 md:w-16">
-              <img
-                v-if="rapport.audit_institution == 'ARMP'"
-                src="~/assets/logos/armp.webp"
-                loading="lazy"
-                fetchpriority="high"
-                alt="Logo ARMP"
-                class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
-                width="60"
-                height="40"
-              />
-              <img
-                v-if="rapport.audit_institution == 'OFNAC'"
-                src="~/assets/logos/ofnac.webp"
-                loading="lazy"
-                fetchpriority="high"
-                alt="Logo OFNAC"
-                class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
-                width="60"
-                height="40"
-              />
-              <img
-                v-if="rapport.audit_institution == 'IGE'"
-                src="~/assets/logos/ige.webp"
-                loading="lazy"
-                fetchpriority="high"
-                alt="Logo IGE"
-                class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
-                width="60"
-                height="40"
-              />
-              <img
-                v-if="rapport.audit_institution == 'Cour des Comptes'"
-                src="~/assets/logos/cour_des_comptes.webp"
-                loading="lazy"
-                fetchpriority="high"
-                alt="Logo Cours des Comptes"
-                class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
-                width="60"
-                height="40"
-              />
-              <img
-                v-if="rapport.audit_institution == 'CENTIF'"
-                src="~/assets/logos/centif.webp"
-                loading="lazy"
-                fetchpriority="high"
-                alt="Logo CENTIF"
-                class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
-                width="60"
-                height="40"
-              />
-              <img
-                v-if="rapport.audit_institution == 'Autres'"
-                src="~/assets/logos/doc.svg"
-                loading="lazy"
-                fetchpriority="high"
-                alt="Logo rapport"
-                class="organisme-logo h-auto w-11 md:w-12 lg:w-14"
-                width="60"
-                height="40"
-              />
-            </div>
-
-            <div class="flex-grow">
-              <p class="text-sm font-normal">{{ rapport.title }}</p>
-              <!-- <p class="text-sm text-gray-500">{{ rapport.description }}</p> -->
-            </div>
-          </NuxtLink>
-        </UCard>
-      </div>
-
-      <div
-        class="flex justify-center border-t border-gray-200 px-3 py-3.5 dark:border-gray-700"
-      >
-        <UPagination
-          v-model="page"
-          size="md"
-          :page-count="pageCount"
-          :total="filteredRapports.length"
-          :active-button="{ color: 'gray' }"
-        />
-      </div>
-    </ClientOnly>
+    <div
+      class="flex justify-end border-t border-gray-200 px-3 py-3.5 dark:border-gray-700"
+    >
+      <UPagination
+        v-model="page"
+        size="md"
+        :page-count="pageCount"
+        :total="filteredRapports.length"
+      />
+    </div>
   </div>
 </template>
 
